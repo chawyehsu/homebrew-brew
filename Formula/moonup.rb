75935d1a5f8101da7b6c490b0adced51df0ddb92
Chawye Hsu <su+git@chawyehsu.com>
2025-06-27 21:57:36 +0800

    moonup: bump to version 0.3.1
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- Formula/moonup.rb
+++ Formula/moonup.rb
@@ -1,5 +1,5 @@
 class Moonup < Formula
-  version "0.3.0"
+  version "0.3.1"
   desc "Manage multiple MoonBit installations"
   homepage "https://github.com/chawyehsu/moonup"
   license "Apache-2.0"
@@ -12,10 +12,10 @@ class Moonup < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/chawyehsu/moonup/releases/download/v#{version}/moonup-aarch64-apple-darwin.tar.gz"
-      sha256 "df34ada5bd11b802e53bf6d4a2483b861d5019045afcd99c69395c6d5d75ebe0"
+      sha256 "addd82bc27ac037beef3a08b56ffb3a3c75e21adc5c9952b70f9747868ac147e"
     else
       url "https://github.com/chawyehsu/moonup/releases/download/v#{version}/moonup-x86_64-apple-darwin.tar.gz"
-      sha256 "21d6023af3d6d5df279e7bc2bf7dd331bc8f3786e14bc73708f4db6fa0542f02"
+      sha256 "50f01a01cb5caa84d604c3b0c182032b2f7e3c282fac08be46989497eaaaab7d"
     end
   end
 
